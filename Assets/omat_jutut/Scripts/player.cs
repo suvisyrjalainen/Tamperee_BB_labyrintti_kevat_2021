388ebe5d9c8c8109ae94c0ca216f3d96a68104b1
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-02-24 15:14:58 +0200

    maa, liikkuminen ja animaatiot

---
+++ Assets/omat_jutut/Scripts/player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController hahmokontrolleri = GetComponent<CharacterController>();
+
+        float horizontal = Input.GetAxis("Horizontal") * 5;
+        float vertical = Input.GetAxis("Vertical") * 5;
+
+        Vector3 nopeus = new Vector3(horizontal, 0, vertical);
+
+        //hahmokontrolleri.SimpleMove(nopeus);
+        hahmokontrolleri.Move(nopeus * Time.deltaTime);
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            anim.SetBool("Walk", true);
+            
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+    }
+}
