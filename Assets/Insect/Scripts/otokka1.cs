7c29cbf236cd1e61acdd31056f6a2b8ca4f06f8f
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-04-14 18:29:07 +0300

    otokan kaantyminen

--- Assets/Insect/Scripts/otokka1.cs
+++ Assets/Insect/Scripts/otokka1.cs
@@ -7,7 +7,7 @@ public class otokka1 : MonoBehaviour
     // Start is called before the first frame update
 
     int layer_index;
-    //private float horisontaalinenPyorinta = 0;
+    public float horisontaalinenPyorinta = 0;
 
     void Start()
     {
@@ -17,20 +17,26 @@ public class otokka1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //eteenpäin meno
         CharacterController hahmokontrolleri = GetComponent<CharacterController>();
         Vector3 nopeus = new Vector3(0, 0, 1);
 
-        
+        //kääntyminen
+        transform.localRotation = Quaternion.Euler(0, horisontaalinenPyorinta, 0);
+        nopeus = transform.rotation * nopeus;
+
+        //kokonaisliikkuminen
         hahmokontrolleri.Move(nopeus * Time.deltaTime);
     }
 
     void OnCollisionEnter(Collision collision)
     {  
-        print("tormays tapahtunut");
+        int kaannos = Random.Range(90, 270);
 
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("wall"))
         {
             print("osuin seinaan");
+            horisontaalinenPyorinta += kaannos;
         }
     }
 
