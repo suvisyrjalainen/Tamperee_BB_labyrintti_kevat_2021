37ed6005d82ba4c7b8210305d6f4934e7f016931
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-04-21 19:38:53 +0300

    ampuminen

---
+++ Assets/omat_jutut/Scripts/ammu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ammu : MonoBehaviour
+{
+    public float luodinNopeus = 10f;
+    public GameObject luotiPrefab;
+    private GameObject ammus = null;
+    Camera FPSCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPSCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ammus = Instantiate(luotiPrefab, transform.position, Quaternion.identity);
+            ammus.GetComponent<Rigidbody>().AddForce(FPSCamera.transform.forward * luodinNopeus, ForceMode.Impulse);
+
+        }
+
+    }
+}
